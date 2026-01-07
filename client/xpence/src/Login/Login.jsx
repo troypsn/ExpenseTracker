9adffa147bcbabbdfe29d8511491e38d89694551
troy <troypsn17@gmail.com>
2026-01-08 00:38:13 +0800

    Added Login Page and did initial testin with axios post -> node server -> mysql database

--- client/xpence/src/Login/Login.jsx
+++ client/xpence/src/Login/Login.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import styles from './Login.module.css';
 
-function LoginSignUp() {
+function Login() {
+
+  const [username, setUsername] = useState("");
+
+
+
+    
+  // Handle form submission
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try{                //axios post request to server
+            const result = await axios.post('http://localhost:5000/login', {
+            username: e.target.username.value,
+            password: e.target.password.value,
+          });
+
+          //returns if user exists in database
+          console.log(result.data);
+          setUsername(`Welcome, ${result.data.data.username}`);
+        } catch (error) {
+          console.error("Login failed", error);
+        }
+        
+    }
+
   return (
-    <div>LoginSignUp</div>
+    <div className={styles.pageContainer}>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.inputContainer}>  
+            <input type="text" placeholder='Username' name="username" required/>
+        </div>
+        <div className={styles.inputContainer}>
+          
+          <input type="password" placeholder='Password' name="password" required/>
+        </div>
+        <button type="submit" className={styles.submitButton}>Login</button>
+      </form>
+      <h1 className={styles.welcomeMessage}>{username}</h1>
+    </div>
   )
 }
 
-export default LoginSignUp
+export default Login
--- server/server.js
+++ server/server.js
@@ -21,9 +21,27 @@ const db = mysql.createConnection({
 
 const sql = "select * from users";
 
+
 app.get('/',(req, res)=>{
     res.status(200).send('Welcome to the Expense Tracker API')
 });
+
+app.post('/login', (req, res)=>{
+    const {username, password} = req.body;
+    db.query(`select count(*) as count from users where username =  "${username}" and password_hash = "${password}"`, (err, result)=>{
+        if (err) {
+            console.error("Database query error", err);
+            return res.status(500).json({success: false, message: "Internal server error"});
+        }
+        if (result[0].count > 0) {
+            console.log(`Username: ${username}, Password: ${password}`);
+            return res.status(200).json({sucess:true, data: {"username" : username, "password" : password}});
+        } else {
+            return res.status(401).json({success: false, message: "Invalid credentials"});
+        }
+    });
+    
+});  
 
 //test connection if it works
 app.get('/test',(req, res)=>{
